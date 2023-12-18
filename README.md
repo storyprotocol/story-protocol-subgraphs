32fbf39cbac6310ecb6c13feaba665da55fd9f0b
Ze Huang <edisonz0718@gmail.com>
2023-12-18 10:57:16 -0800

    Add README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # story-protocol-subgraphs
-All subgraphs definition for Story Protocol
+
+## Set up
+To use a specific version of the subgraph, go to the subgraphs/(your version) folder and do:
+1. Set up the .env.js file based on the .env.example.js
+2. `npm run codegen` : This will generate **subgraph.yaml** based on **template.yaml** and also generate the code based on the schema.graphql 
+3. `npm run build` : This will build the code in /src folder.  
+4. `npm run deploy` : This will deploy the code to the graph
---
+++ subgraphs/v0-alpha/.env.example.js
@@ -0,0 +1,27 @@
+module.exports = {
+    network: "sepolia",
+    ipOrgController: {
+        address: "<ADDRESS>",
+        startBlock: 4845597 // Replace with your start block  
+    },
+    moduleRegistry: {
+        address: "<ADDRESS>",
+        startBlock: 4845597 // Replace with your start block
+    }, 
+    licenseRegistry: {
+        address: "<ADDRESS>",
+        startBlock: 4845597 // Replace with your start block
+    },
+    licenseModule: {
+        address: "<ADDRESS>",
+        startBlock: 4845597 // Replace with your start block
+    },
+    registrationModule: {
+        address: "<ADDRESS>",
+        startBlock: 4845597 // Replace with your start block
+    },
+    relationshipModule: {
+        address: "<ADDRESS>",
+        startBlock: 4845597 // Replace with your start block
+    }
+}
